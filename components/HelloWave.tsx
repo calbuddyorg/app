9b375c2bd5f0737276734f9952f894fa6ad80b0d
Franklin Roa Guerrero <franklinroaguerrero@gmail.com>
2024-07-07 19:39:49 -0500

    remove

--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -22,9 +22,9 @@ export function HelloWave() {
   }));
 
   return (
-    <Animated.View style={animatedStyle}>
-      <ThemedText style={styles.text}>👋</ThemedText>
-    </Animated.View>
+    // <Animated.View style={animatedStyle}>
+    //   <ThemedText style={styles.text}>👋</ThemedText>
+    // </Animated.View>
   );
 }
 
